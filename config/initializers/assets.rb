b0733c0d94149e19097a738070c61df32216f2a5
WIND Site <thomascaldwell1@hotmail.co.uk>
2016-04-13 21:46:41 +0100

    LinkedIn link and about page picture

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( home_image.jpg)
-Rails.application.config.assets.precompile += %w( wind_logo.png )
+Rails.application.config.assets.precompile += %w( about_img.jpg )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
