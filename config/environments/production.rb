363dadcbd2c254d750fccbb6df37ad5ecdf5ad81
WIND Site <thomascaldwell1@hotmail.co.uk>
2016-05-25 16:00:00 +0100

    Fixing contact form issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,17 @@ Rails.application.configure do
 
   # Devise
   config.action_mailer.default_url_options = { :host => 'http://tomcaldwell.co.uk' }
+  config.actionMailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   Dynopoker.configure do |config|
       config.address = 'http://wakemydyno.com'
