d5baf5a2325d47cce941899e0476733b534cb121
WIND Site <thomascaldwell1@hotmail.co.uk>
2016-05-25 13:36:02 +0100

    route issue 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,6 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'http://tomcaldwell.co.uk/', :protocol => 'https' }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -80,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Devise
-  config.action_mailer.default_url_options = { :host => 'tomcaldwellweb.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://tomcaldwell.co.uk' }
 
   Dynopoker.configure do |config|
       config.address = 'http://wakemydyno.com'
