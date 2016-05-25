7faa41f9945db03c25151bda5423e7a4355f53b3
WIND Site <thomascaldwell1@hotmail.co.uk>
2016-05-25 16:14:53 +0100

    Contact form edit again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Devise
-  config.action_mailer.default_url_options = { :host => 'http://tomcaldwell.co.uk' }
+  config.action_mailer.default_url_options = { host: 'http://tomcaldwell.co.uk' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
